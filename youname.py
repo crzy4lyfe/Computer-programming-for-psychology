d1a6e2e6a7745fc31ac933eda7bad4afe57331cc
crzy4lyfe <113379037+crzy4lyfe@users.noreply.github.com>
2022-09-25 13:42:34 -0600

    Add files via upload

---
+++ youname.py
@@ -0,0 +1,32 @@
+#print exercises
+
+print('j')
+print('o')
+print('l')
+print('i')
+print('e')
+#2. No variables show up in the variable editor. This is because no variables were assigned. 
+
+
+#Operation exercises 
+
+print(5/2)
+print(5.0/2.0)
+#python outputs the same value for these operations
+print(5%2)
+print(10%6) 
+print(7%11)
+#the modulo operator(%) gives the remainder of division 
+print(5**2)
+print(10**6)
+print(7**11)
+#the (**) operator does exponentation 
+print(5//2)
+print(10//6)
+print(7//11)
+#the (//) operator does division and gives us the whole number rounded down 
+print(5+2+10*6/7)
+print(5+2+(10*(6/7)))
+print(24+7+10*15/4)
+print(24+7+(10*(15/4)))
+#python does follow order of operations.
